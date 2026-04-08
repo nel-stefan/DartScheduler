4d2920e194f0a571930765783d82dea8d0c4119e
Stefan Marchal <stefanmarchal@hotmail.com>
2026-04-08 15:46:29 +0200

    feat: typeahead timeout dropdown verhoogd naar 1 seconde

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { MAT_SELECT_CONFIG } from '@angular/material/select';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    { provide: MAT_SELECT_CONFIG, useValue: { typeaheadDebounceInterval: 1000 } },
+  ],
 };
